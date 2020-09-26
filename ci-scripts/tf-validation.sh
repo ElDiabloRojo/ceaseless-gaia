d23bb5f0be7b007b91a4c72c6316d946730f40e0
Sam Holden <sholden@worth.systems>
2020-09-26 16:14:16 +0100

    missing underscore...

--- ci-scripts/tf-validation.sh
+++ ci-scripts/tf-validation.sh
@@ -8,7 +8,7 @@ echo $1
 
 WORKDIR=$TRAVIS_BUILD_DIR/environments/$1
 
-cd $WORK_DIR
+cd $WORKDIR
 echo "Creating change plan"
 terraform init $tf_init_cli_options
 terraform validate $tf_validation_cli_options
