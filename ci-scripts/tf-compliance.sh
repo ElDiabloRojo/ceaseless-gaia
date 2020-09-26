4afb75897d1723ff36e1eab782adf13d4d246662
Sam Holden <sholden@worth.systems>
2020-09-26 22:43:43 +0100

    debugging plan file

--- ci-scripts/tf-compliance.sh
+++ ci-scripts/tf-compliance.sh
@@ -4,4 +4,7 @@ set -x
 
 terraform init $TF_SOURCE_DIR
 terraform plan -out $TF_PLAN_FILE --var-file=$TF_SOURCE_DIR/terraform.tfvars $TF_SOURCE_DIR
+
+cat $TF_PLAN_FILE
+
 docker run --rm -v $PWD:/target -it eerkunt/terraform-compliance -f tf-compliance -p $TF_PLAN_FILE
