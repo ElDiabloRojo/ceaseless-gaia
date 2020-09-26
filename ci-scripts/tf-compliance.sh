a0b267e028d8db60b4d637a92b00d92f90da8a52
Sam Holden <sholden@worth.systems>
2020-09-26 22:37:37 +0100

    providing tfvars to compliance plan

--- ci-scripts/tf-compliance.sh
+++ ci-scripts/tf-compliance.sh
@@ -3,5 +3,5 @@
 set -x
 
 terraform init $TF_SOURCE_DIR
-terraform plan -out $TF_PLAN_FILE $TF_SOURCE_DIR
+terraform plan -out $TF_PLAN_FILE --var-file=$TF_SOURCE_DIR/terraform.tfvars $TF_SOURCE_DIR
 docker run --rm -v $PWD:/target -it eerkunt/terraform-compliance -f tf-compliance -p $TF_PLAN_FILE
