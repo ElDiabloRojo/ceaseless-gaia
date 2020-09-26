ae6156ca78526484febba98ef94e51b24d269924
Sam Holden <sholden@worth.systems>
2020-09-26 16:46:40 +0100

    commenting ci scripts.

--- ci-scripts/tf-apply.sh
+++ ci-scripts/tf-apply.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# This script is used to run the terraform apply subcommand, and will deploy 
+# a given environments IaC source.
+#
+
 set -x
+
+# [TODO: move path of workdir out of this script, we only want an input param]
 
 WORK_DIR=$TRAVIS_BUILD_DIR/environments/$1
--- ci-scripts/tf-validation.sh
+++ ci-scripts/tf-validation.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+#
+# This script is used to run the terraform validation subcommand against a 
+# given environments IaC directory. Validate contributes to static analysis and
+# ensures the terraform code is syntactically correct.
+#
+
 set -x
+
+# [TODO: move path of workdir out of this script, we only want an input param]
 
 WORK_DIR=$TRAVIS_BUILD_DIR/environments/$1
 
